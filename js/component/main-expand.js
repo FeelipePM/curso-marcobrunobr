5e3b9cf9e5e05f3149e836c84beee64ba23d002f
Felipe Machado <Feelipe@email.com>
2018-08-22 15:59:33 -0300

    Efeito de ampliar e diminuir imagem no portfolio

--- js/component/main-expand.js
+++ js/component/main-expand.js
@@ -1,7 +1,15 @@
 var mainExpand = window.document.querySelector(".main-expand");
+var mainExpandSubtitle = mainExpand.querySelector(".subtitle");
+var mainCard = window.document.querySelector(".main-card");
 
 function clickMainExpand() {
-  console.log("Funcionou");
+  if (mainCard.classList.contains("-isActive")) {
+    mainCard.classList.remove("-isActive");
+    mainExpandSubtitle.textContent = "Full Screen";
+  } else {
+      mainCard.classList.add("-isActive");
+      mainExpandSubtitle.textContent = "Short Screen";
+  }
 }
 
 mainExpand.addEventListener("click", clickMainExpand);
