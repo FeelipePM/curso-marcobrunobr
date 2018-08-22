c7179ab64dee76b23bea1e8173eb880e5797a594
Felipe Machado <Feelipe@email.com>
2018-08-22 00:24:14 -0300

    Começando a criar a parte de JS

---
+++ js/component/main-expand.js
@@ -0,0 +1,7 @@
+var mainExpand = window.document.querySelector(".main-expand");
+
+function clickMainExpand() {
+  console.log("Funcionou");
+}
+
+mainExpand.addEventListener("click", clickMainExpand);
